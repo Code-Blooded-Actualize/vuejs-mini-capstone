e68612ca02216d1a5f90c9be7bb67f13406c372f
JackMarx <seriousfools@gmail.com>
2018-10-29 17:05:51 -0500

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-md-4" v-for="product in products">
+        <h1>{{ product.name }}</h1>
+        <h3>{{ product.formatted.price }}</h3>
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" v-bind:src="product.default_image_url" v-bind:alt="product.name">
+          <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">{{ product.description }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      products: [{formatted:{}, images: [{url: "", alt: ""}]}]
+    };
+  },
+  created: function() {
+    axios
+    .get("/api/products")
+    .then(response => {
+      console.log(response.data)
+      this.products = response.data;
+    });
+  },
+  methods: {},
+  computed: {}
+};
 </script>
